56b4d0c436766dbd95d0bc274b4bc6a8f9debf4d
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2023-11-05 19:58:43 +0100

    tests avec diff luminosite withoutIA.py

--- withoutIA.py
+++ withoutIA.py
@@ -10,17 +10,39 @@ import machine
 sensor.reset()  # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
-sensor.skip_frames(time=2000)  # Wait for settings take effect.
 clock = time.clock()
+interesting_colors = [(0, 100, 32, 71, 23, 51)] #[(50, 90, 51, 36, 1, 26)] #[(30, 89, 66, 11, 54, 24)] #[(50, 90, 51, 36, 1, 26)]
 roi = (30, 0, 256, 230) #region of interest
-interesting_colors = [(30, 89, 66, 11, 54, 24)] #[(50, 90, 51, 36, 1, 26)]
 sensor.set_windowing(roi)
+start_x, start_y, width, height = roi
+w = int(width/2)
+h = int(height/2)
+z1_roi = (start_x, start_y, w, h)
+z2_roi = (start_x + w, start_y, w, h)
+z3_roi = (start_x + w, start_y + h, w, h)
+z4_roi = (start_x, start_y + h, w, h)
+del start_x, start_y, width, height, w, h #mettre dans une fonction pour éviter ça ?
 
-while True:
-    clock.tick()
-    img = sensor.snapshot()
-    """
-    binary_mask = img.binary(interesting_colors)
+sensor.skip_frames(time=2000)  # Wait for settings take effect.
+
+def find_ball_blobs(
+    colors,
+    roi
+    ):
+    for blob in img.find_blobs(
+      colors,
+      roi= roi,
+      area_threshold=10,
+      merge=False,
+      ):
+      img.draw_rectangle(blob.rect(), color=(0,255,0))
+      img.draw_cross(blob.cx(), blob.cy(), color=(0,255,0))
+
+def find_ball_circle(
+    colors,
+    roi
+    ):
+    binary_mask = img.binary(colors)
 
     for c in binary_mask.find_circles(
       roi= roi,
@@ -32,14 +54,23 @@ while True:
       r_max= 10,
       ):
       img.draw_circle(c.x(), c.y(), c.r(), color= (255, 0, 0))
-    """
-    for blob in img.find_blobs(
-      interesting_colors,
-      roi= roi,
-      area_threshold=10,
-      merge=False,
-      ):
-      img.draw_rectangle(blob.rect(), color=(0,255,0))
-      img.draw_cross(blob.cx(), blob.cy(), color=(0,255,0))
 
-    print(f"FPS : {clock.fps()}")
+while True:
+    clock.tick()
+    img = sensor.snapshot()
+
+    z1 = img.copy(roi=z1_roi)
+    z2 = img.copy(roi=z2_roi)
+    z3 = img.copy(roi=z3_roi)
+    z4 = img.copy(roi=z4_roi)
+    z1_lum = z1.get_statistics().l_mean()
+    z2_lum = z2.get_statistics().l_mean()
+    z3_lum = z3.get_statistics().l_mean()
+    z4_lum = z4.get_statistics().l_mean()
+
+    print(z1_lum)
+    if z1_lum < 50:
+        find_ball_blobs([(0, 100, 28, 82, 20, 66)], z1_roi)
+    else:
+        find_ball_blobs([(31, 83, 22, 44, -6, 46)], z1_roi)
+    #print(f"FPS : {clock.fps()}")
