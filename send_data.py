0b1e1ce81f2b923036d9509770634c0f1ef03cac
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2023-12-29 12:08:46 +0100

    correc bug

--- send_data.py
+++ send_data.py
@@ -4,18 +4,17 @@ import sensor
 import time
 import machine
 import pyb
-from random import randrange
+from random import randint, uniform
 
 #P4 (TX)
 #P5 (RX)
 
 uart = pyb.UART(3, 115200)
 
-def r() {
-    return round(randrange(0, 150), 5)
-}
+def r():
+    return round(uniform(0, 150), randint(0, 5))
 
 while True:
    uart.write(
-   f"b{r()}, {r()}m{r()}, {r()}p{r()}, {r()}"
+   f"b{r()},{r()}m{r()},{r()}p{r()},{r()}"
 )
