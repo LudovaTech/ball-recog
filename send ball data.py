ca6bdf606bd14109c424631043a4cf194a06a58b
RemyMagnon <magnonremy@gmail.com>
2024-04-04 09:02:35 +0200

    all

--- send ball data.py
+++ send ball data.py
@@ -1,49 +1,60 @@
-import sensor, image, time, math, machine
+import sensor, image, time, math, machine, pyb
 
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
-sensor.set_auto_gain(False)
-sensor.set_auto_whitebal(False)
-sensor.skip_frames(time = 2000)
-sensor.set_brightness(3)
-sensor.set_saturation(3)
-sensor.set_contrast(3)
-#sensor.set_windowing(0, 0, 270, 200)
+#sensor.__write_reg(0, write_reg) # Modifie le deuxième paramètre pour augmenter/diminuer la luminosité
+sensor.set_brightness(0) # Entre -3 et 3 pour augmenter/diminuer la luminosité
+sensor.set_saturation(-2)
+sensor.set_contrast(-3)
+sensor.skip_frames(time = 500)
 clock = time.clock()                # Create a clock object to track the FPS.
 
-uart = machine.UART(1, 115200)
+uart = pyb.UART(3, 115200)
 
-def send_data(uart, bx, by, gx, gy):
-    uart.write(f"b{bx},{by}m{gx},{gy}p{0},{0}")
-
-ball_x = 0
-ball_y = 0
-goal_x = 0
-goal_y = 0
-
-offset_x = 13
-offset_y = 20
-
-img_width = int(sensor.width()/2)
-img_height = int(sensor.height()/2)
+robot = 8
+if robot == 7:
+    offset_x = 8
+    offset_y = 20
+elif robot == 8:
+    offset_x = 7
+    offset_y = 5
 
 while(True):
     clock.tick()
     img = sensor.snapshot()
 
-    img.draw_circle(img_width + offset_x, img_height + offset_y, 35, fill = True, color = (0,0,0))
-    img.draw_circle(img_width + offset_x, img_height + offset_y, 210, fill = False, color = (0,0,0), thickness = 150)
+    ball_x = 0
+    ball_y = 0
+    enemy_goal_y = 0
+    enemy_goal_x = 0
+    my_goal_x = 0
+    my_goal_y = 0
 
-    for blob in img.find_blobs([(0, 100, 16, 127, -128, 125)], pixels_threshold=1, merge=True):
+    img.draw_circle(int(sensor.width()/2) + offset_x, int(sensor.height()/2) - offset_y, 35, fill = True, color = (0,0,0))
+    img.draw_circle(int(sensor.width()/2) + offset_x, int(sensor.height()/2) - offset_y, 200, fill = False, color = (0,0,0), thickness = 150)
+
+    for blob in img.find_blobs([(27, 100, 22, 127, -128, 127)], pixels_threshold=10, merge=True):
         img.draw_rectangle(blob.rect())
-        ball_x = blob.x() - img_width - offset_x
-        ball_y = -blob.y() + img_height + offset_y
+        # Changement de repère avec l'origine au centre du robot et l'axe x inversé
+        ball_x  = -(blob.cx() - int(sensor.width()/2) - offset_x)
+        ball_y = -blob.cy() + int(sensor.height()/2) - offset_y
 
-    for blob in img.find_blobs([(0, 22, -128, -7, -127, -4)], pixels_threshold=50, merge=True):
+    for blob in img.find_blobs([(35, 100, -128, -4, 3, 127)], pixels_threshold=80, merge=True):
         img.draw_rectangle(blob.rect())
-        goal_x = blob.x() - img_width - offset_x
-        goal_y = -blob.y() + img_height + offset_y
+        # Changement de repère avec l'origine au centre du robot et l'axe x inversé
+        enemy_goal_x = -blob.cx() + int(sensor.width()/2) + offset_x
+        enemy_goal_y = -blob.cy() + int(sensor.height()/2) - offset_y
 
-    send_data(uart, -ball_x, ball_y, -goal_x, goal_y)
-    print(f"b{-ball_x},{ball_y}m{-goal_x},{goal_y}p{0},{0}")
+    for blob in img.find_blobs([(0, 0, -128, -128, -128, -128)], pixels_threshold=80, merge=True):
+        img.draw_rectangle(blob.rect())
+        # Changement de repère avec l'origine au centre du robot et l'axe x inversé
+        my_goal_x = -blob.cx() + int(sensor.width()/2) + offset_x
+        my_goal_y = -blob.cy() + int(sensor.height()/2) - offset_y
+
+    # Send data
+    data = f"b{ball_x},{ball_y}g{my_goal_x},{my_goal_y}G{enemy_goal_x},{enemy_goal_y}"
+    uart.write(data)
+    print(data)
+    #print(math.sqrt((enemy_goal_x*enemy_goal_x) + (enemy_goal_y*enemy_goal_y)))
+
