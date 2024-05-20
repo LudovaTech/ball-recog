faa30f8dd32e26228266b0d25092c7e89956e019
RemyMagnon <magnonremy@gmail.com>
2024-05-20 23:10:53 +0200

    Envoi des coordonnées réelles des objets par rapport au robot et clignotement de la LED

--- send ball data.py
+++ send ball data.py
@@ -23,46 +23,84 @@ sensor.set_contrast(-2)
 
 sensor.skip_frames(time = 300)
 
+uart = pyb.UART(3, 115200)
+led = machine.LED("LED_GREEN")
+
 robot = "SN9"
 if robot == "SN9":
-    offset_x = 8
-    offset_y = 20
-    uart = pyb.UART(3, 115200)
+    offset_x = 10
+    offset_y = 11
 elif robot == "SN10":
     offset_x = 7
     offset_y = 5
-    uart = pyb.UART(1, 115200)
+
+
+def realX(x):
+    return -x + sensor.width()//2 + offset_x
+
+def realY(y):
+    return -y + sensor.height()//2 - offset_y
+
+# TODO : corriger la formule
+def getRealDistance(x, y):
+    return 0.68 * math.pow(2.71828, 0.046 * math.sqrt(x**2 + y**2)) + 8
+
+def getAngle(x, y):
+    if x == 0:
+        if y == 0:
+            angle = 0
+        else:
+            angle = abs(y)/y * math.pi/2
+    else:
+        angle = math.atan2(y, x)
+
+    return angle
+
+def getRealCoord(x, y):
+    angle = getAngle(x, y)
+    distance = getRealDistance(x, y)
+    return distance * math.cos(angle), distance * math.sin(angle)
 
 
 # TODO : blob filtering
 def detectBlob(colorThreshold, pixelNumberThreshold, showBlob=True):
-    blobX = 0
-    blobY = 0
+    blobX, blobY = (0, 0)
     for blob in img.find_blobs([colorThreshold], pixels_threshold=pixelNumberThreshold, merge=False):
         if showBlob:
             img.draw_rectangle(blob.rect())
-        blobX = -(blob.cx() - sensor.width()//2 - offset_x)
-        blobY = -blob.cy() + sensor.height()//2 - offset_y
-    return (blobX, blobY)
+        blobX, blobY = getRealCoord(realX(blob.cx()), realY(blob.cy()))
+    return round(blobX), round(blobY)
 
+compteur = 0
 
 while(True):
     time.clock().tick()
     img = sensor.snapshot()
 
-    # Réglages luminosité pour la RT
+    compteur += 1
+    if compteur%8 == 0:
+        led.on()
+        compteur = 0
+    else:
+        led.off()
+
+    # Réglages de luminosité pour la RT
     #img.gamma(gamma=1.0, contrast=1.2, brightness=0)
 
-    ballCoordinates = detectBlob((27, 100, 22, 127, -128, 127), 10, True)
-    myGoalCoordinates = detectBlob((35, 100, -128, -4, 3, 127), 80, False)
-    enemyGoalCoordinates = detectBlob((0, 0, -128, -128, -128, -128), 80, False)
+    ballCoord = detectBlob((27, 100, 22, 127, -128, 127), 10, True)
+    myGoalCoord = detectBlob((35, 100, -128, -4, 3, 127), 80, False)
+    enemyGoalCoord = detectBlob((0, 0, -128, -128, -128, -128), 80, False)
 
     # Send data
-    data = "b{:+04d}{:+04d}{:+04d}{:+04d}{:+04d}{:+04d}e".format(ballCoordinates[0],
-                                                                 ballCoordinates[1],
-                                                                 myGoalCoordinates[0],
-                                                                 myGoalCoordinates[1],
-                                                                 enemyGoalCoordinates[0],
-                                                                 enemyGoalCoordinates[1])
+    #data = f"b{ballCoord[0]},{ballCoord[1]}g{myGoalCoord[0]},{myGoalCoord[1]}G{enemyGoalCoord[0]},{enemyGoalCoord[1]}"
+
+    data = "b{:+04d}{:+04d}{:+04d}{:+04d}{:+04d}{:+04d}e".format(ballCoord[0],
+                                                                 ballCoord[1],
+                                                                 myGoalCoord[0],
+                                                                 myGoalCoord[1],
+                                                                 enemyGoalCoord[0],
+                                                                 enemyGoalCoord[1])
     uart.write(data)
     print(data)
+    img.draw_circle(realX(0), realY(0), 32, (0, 0, 0), fill=True)
+    img.draw_circle(realX(0), realY(0), 1)
